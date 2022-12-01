8a7facd8ca60a275f6b8238943708320a504dea0
KarlaG56 <213356@ids.upchiapas.edu.mx>
2022-12-01 11:10:29 -0600

    [UPDATE] Login and register

--- src/components/Alimentos/Formulario.jsx
+++ src/components/Alimentos/Formulario.jsx
@@ -1,59 +1,52 @@
 function Formulario() {
     return (
-            <div className='body-Aliments'>
+        <div className='body-Aliments'>
+            <br /><br />
+            <form className="container-alimentos">
+                <br />
+                <h1 className="" >Ingredientes</h1>
+                <br />
+
+                <div >
+                    <div id="div-text">
+                        <label id="text-aliment">Nombre</label>
+                        <label id="text-aliment2">Gramos</label>
+                    </div>
+                    <div id="div-input">
+                    <input id="text-aliment" type="text" name="nombre"  required />
+                    <input id="text-aliment2" type="text" name="gramos"  required />
+                    </div>
+                </div>
+
+                <div >
+                    <div id="div-text">
+                        <label id="text-aliment">Nombre</label>
+                        <label id="text-aliment2">Gramos</label>
+                    </div>
+                    <div id="div-input">
+                    <input id="text-aliment" type="text" name="nombre"  required />
+                    <input id="text-aliment2" type="text" name="gramos"  required />
+                    </div>
+                </div>
+
+                <div >
+                    <div id="div-text">
+                        <label id="text-aliment">Nombre</label>
+                        <label id="text-aliment2">Gramos</label>
+                    </div>
+                    <div id="div-input">
+                    <input id="text-aliment" type="text" name="nombre"  required />
+                    <input id="text-aliment2" type="text" name="gramos"  required />
+                    </div>
+                </div>
+
+                <button className="button-more" >+</button>
+                <button className="button-aliment" >Calcular</button>
                 <br /><br />
-                <form className="container-alimentos">
-                    <br/>
-                    <h1 className="" >Ingredientes</h1>
-                    <br />
+            </form>
+            <br /><br /><br />
+        </div>
 
-                    <div >
-                        <label id="text-register">Nombre</label>
-                        <input className="place" type="text" name="nombre" id='Nombre' required/>
-                        <input className="place" type="text" name="nombre" id='Nombre' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Edad</label>
-                        <input className="place" type="text" name="edad" id='edad' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Genero</label>
-                        <input className="place" type="text" name="genero" id='genero' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Correo electronico</label>
-                        <input className="place" type="email" name="email" id='email' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Contraseña</label>
-                        <input className="place" type="password" name="password" id='password_Login' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Confirmar contraseña</label>
-                        <input className="place" type="password" name="cpassword" id='cpassword' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Peso</label>
-                        <input className="place" type="text" name="weight" id='weight' required/>
-                    </div>
-
-                    <div >
-                        <label id="text-register">Altura</label>
-                        <input className="place" type="text" name="height" id='height' required/>
-                    </div>
-
-                    <button className="button-register" >Registrarse</button>
-                    <br /><br />
-                </form>
-                <br/><br /><br />
-            </div>
-     
 
     );
 

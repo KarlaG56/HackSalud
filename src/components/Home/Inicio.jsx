f65fe3b0e458a6bf89e8a4fb8f1bd08172abfb58
KarlaG56 <213356@ids.upchiapas.edu.mx>
2022-12-01 07:19:12 -0600

    Merge Anel

--- src/components/Home/Inicio.jsx
+++ src/components/Home/Inicio.jsx
@@ -1,38 +1,34 @@
-import imagen from "../../../public/img/Frase.png"
-import like from "../../../public/img/like.png"
-import comentarios from "../../../public/img/comentarios.png"
-import profesionales from "../../../public/img/usuarios.png"
-import personas from "../../../public/img/personas.png"
+
 
 
 function Inicio() {
     return ( 
-        <div className="body">
+        <div className="body-container">
             <br /><br />
             <div className="Frase">
-                <img src={imagen}/>
+                <img src="/img/Frase.png"/>
             </div>
             <div className="Expertos">
-                <img src={like}/>
+                <img src="/img/like.png"/>
                 <br />
                 <h3 className="SubTituloHome">Expertos reconocidos</h3>
                 <br />
                 <a className="TextoSubtitulo">Colaboramos con los más importantes medios de comunicación como expertos en nutrición. Consulta nuestras más de 400 apariciones en medios.</a>
             </div>
             <div className="Profesionales">
-                <img src={profesionales}/>
+                <img src="/img/usuarios.png"/>
                 <h3 className="SubTituloHome">Grandes profesionales a tu servicio</h3>
                 <br />
                 <a className="TextoSubtitulo">Un equipo de dietistas-nutricionistas, médicos endocrinos, psicólogos y coaches que trabajan para que tú consigas tus objetivos</a>
             </div>
             <div className="Pacientes">
-                <img src={comentarios}/>
+                <img src="/img/comentarios.png"/>
                 <h3 className="SubTituloHome">+9000 pacientes satisfechos</h3>
                 <br />
                 <a className="TextoSubtitulo">Somos expertos en nutrición con una gran experiencia. Cosulta los testimonios de nuestros pacientes.</a>
             </div>
             <div className="DietasPersonalizadas">
-                <img src={personas}/>
+                <img src="/img/personas.png"/>
                 <h3 className="SubTituloHome">Dietas personalizadas</h3>
                 <br />
                 <a className="TextoSubtitulo">Para conseguir tus objetivos de peso, salud o rendimiento deportivo, la dieta se tiene que adaptar a ti y no al revés</a>
